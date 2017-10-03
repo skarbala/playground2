03b7dd9d4c5b56276c4e025338ee07684d3823c1
martin.skarbala <martin.skarbala@itera.no>
2017-10-03 14:57:02 +0200

    added new page

--- partials/head.php
+++ partials/head.php
@@ -1,8 +1,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ihrisko</title>
+    <title>Ihrisko II</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
 </head>
