e46e958b75aec8b9baec13878e0fbae8dd3d78da
martin.skarbala <martin.skarbala@itera.no>
2017-10-04 12:09:31 +0200

    added minions

--- minions.php
+++ minions.php
@@ -3,42 +3,52 @@
 <!doctype html>
 <html lang="en">
 <?php include 'partials/head.php'; ?>
+<link href="https://fonts.googleapis.com/css?family=Secular+One" rel="stylesheet">
 <body>
 <?php include 'partials/navigation.php' ?>
 <style>
+    body{
+        background: url("assets/img/minions_background.jpg")  no-repeat center center fixed;
+        background-size: cover;
+    }
     h1 {
-        font-family: 'Gloria Hallelujah', cursive;
-        text-transform: uppercase;
-        font-size: 50px;
+        font-family: 'Secular One', sans-serif;
+        font-size: 70px;
         font-weight: 600;
-        color: #be2d2b;
+        color: #ffc832;
+        text-shadow: 2px 2px #ec971f;
     }
 
-    ul.minions {
+    button.btn-warning{
+        background-color: #ffc832;
+        border:none;
+        border-radius: 0;
+    }
+    input.form-control{
+        border-radius: 0;
+    }
+    div.minions {
         list-style: none;
     }
 
-    img {
-        width: 70px;
-        margin:20px;
-    }
-
-    ul.minions li {
+    div.minions img {
         float: left;
         margin: 10px auto;
+        width: 80px;
+        height: 140px;
     }
 </style>
 </body>
 <div class="container">
     <div class="text-center">
-        <h1>Minion time</h1>
-        <div class="col-md-8 col-md-offset-2">
+        <h1>minion time</h1>
+        <div class="col-md-6 col-md-offset-3">
             <input type="number" class="form-control">
             <button class="btn btn-warning btn-block">Get them</button>
         </div>
-        <div class="col-md-8 col-md-offset-2">
-            <ul class="minions">
-            </ul>
+        <div class="col-md-10 col-md-offset-1">
+            <div class="minions">
+            </div>
         </div>
     </div>
 
@@ -48,12 +58,17 @@
         for (i = 0; i < $('input').val(); i++) {
             setTimeout(function () {
                 addMinion($('.minions'));
-            }, 1000 * i);
+            }, 300 * i);
 
         }
     });
     function addMinion(element) {
-        element.append('<li><img src="assets/img/minion.png" alt=""></li>');
+        element.append('<li><img src="assets/img/minions/' + getRandomMinionImage() + '" alt=""></li>');
+    }
+
+    function getRandomMinionImage() {
+        var number = Math.round(Math.random() * (5 - 1) + 1);
+        return number + ".png"
     }
 </script>
 </html>
