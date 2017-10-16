a17eb5d73a75403cf404308951d3650b00b396d9
martin.skarbala <martin.skarbala@itera.no>
2017-10-16 15:12:57 +0200

    refactor js

--- js/sin-city.js
+++ js/sin-city.js
@@ -1,17 +1,26 @@
 var detail = $('div.detail article');
-
+var loader = $('.loader');
 
 $("li.sin").click(function (event) {
     event.preventDefault();
     var key = $(this).find('a').attr("data");
-    $('.loader').fadeIn();
-    $(detail).hide();
+    loader.fadeIn();
+    detail.hide();
     setTimeout(function () {
         loadDetail(key);
     }, 800);
 
 });
 
+function fillInDetail(data) {
+    detail.find('h4').text(data.author + " : " + data.title);
+    detail.find('p').text(data.message);
+    detail.find('h4').append("<span>" + data.creation_date + "</span>");
+    var tags = detail.find('ul').html('');
+    data.tags.forEach(function (element) {
+        tags.append('<li>' + element + '</li>');
+    });
+}
 function loadDetail(key) {
     $.ajax({
         url: baseURL + 'get-detail.php',
@@ -19,15 +28,9 @@ function loadDetail(key) {
         dataType: "json"
     })
         .done(function (data) {
-            $('.loader').fadeOut(200);
+            loader.fadeOut(200);
+            fillInDetail(data);
             detail.fadeIn(500);
-            detail.find('h4').text(data.author + " : " + data.title);
-            detail.find('p').text(data.message);
-            detail.find('h4').append("<span>" + data.creation_date + "</span>");
-            var tags = detail.find('ul').html('');
-            data.tags.forEach(function (element) {
-                tags.append('<li>' + element + '</li>');
-            });
 
         })
         .fail(function (msg) {
