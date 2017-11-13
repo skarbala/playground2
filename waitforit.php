8e47f4d92e9d837505a7eebc3ea74f74cfd44eba
martin.skarbala <martin.skarbala@itera.no>
2017-11-13 12:49:47 +0100

    blured

--- waitforit.php
+++ waitforit.php
@@ -6,69 +6,76 @@
 <body>
 <?php include 'partials/navigation.php' ?>
 <style>
-    body {
-        background-color: #f5f5f5;
-    }
+  body {
+    background-color: #f5f5f5;
+  }
 
-    h1 {
-        text-transform: uppercase;
-        font-size: 50px;
-        font-weight: 600;
-        color: #be2d2b;
-        text-shadow: 2px 2px #9c2424;
-        font-family: 'Gloria Hallelujah', cursive;
-    }
-    
-    .current-wait-time p {
-        font-size: 30px;
-        font-weight: 600;
-    }
+  h1 {
+    text-transform: uppercase;
+    font-size: 50px;
+    font-weight: 600;
+    color: #be2d2b;
+    text-shadow: 2px 2px #9c2424;
+    font-family: 'Gloria Hallelujah', cursive;
+  }
 
-    .current-wait-time span {
-        color: #be2d2b;
-    }
+  .current-wait-time p {
+    font-size: 30px;
+    font-weight: 600;
+  }
 
-    .error {
-        border: 2px solid #be2d2b;;
-    }
+  .current-wait-time span {
+    color: #be2d2b;
+  }
 
-    div.current-wait-time {
-        margin-top: 50px;
-    }
+  .error {
+    border: 2px solid #be2d2b;;
+  }
 
-    div.row {
-        margin: 30px 0;
-    }
+  div.current-wait-time {
+    margin-top: 50px;
+  }
+
+  div.row {
+    margin: 30px 0;
+  }
 
 </style>
 </body>
 <div class="container">
-    <div class="text-center">
-        <h1>wait for it !</h1>
-    </div>
-    <div class="col-sm-6 col-sm-offset-3">
-        <div class="row">
-            <h2>Its gonna be legen...</h2>
-            <div class="input-group">
+  <div class="text-center">
+    <h1>wait for it !</h1>
+  </div>
+  <div class="col-sm-6 col-sm-offset-3">
+    <div class="row">
+      <h2>Its gonna be legen...</h2>
+      <div class="input-group">
                 <span class="input-group-btn">
                 <button class="btn-primary btn" type="button" id="startWaitForText">wait for it</button>
             </span>
-                <input type="text" class="form-control" id="waitForTextInput" placeholder="Your text will appear here">
-            </div>
-        </div>
-        <div class="row">
-            <h2>Wait for class</h2>
-            <div class="input-group">
+        <input type="text" class="form-control" id="waitForTextInput" placeholder="Your text will appear here">
+      </div>
+    </div>
+    <div class="row">
+      <h2>Wait for class</h2>
+      <div class="input-group">
                 <span class="input-group-btn">
                 <button class="btn-primary btn" type="button" id="startWaitForProperty">Get the property</button>
             </span>
-                <input type="text" class="form-control" id="waitForProperty"
-                       placeholder="This input will get a class">
-            </div>
-        </div>
-
-        <div class="current-wait-time text-center"></div>
+        <input type="text" class="form-control" id="waitForProperty"
+               placeholder="This input will get a class">
+      </div>
     </div>
+    <div class="row">
+      <h2>Blur!</h2>
+      <div class="input-group">
+        <input type="text" class="form-control" id="waitForBlur"
+               placeholder="This input will get a class">
+      </div>
+    </div>
+
+    <div class="current-wait-time text-center"></div>
+  </div>
 </div>
 <script>
     $('#startWaitForText').click(function () {
@@ -84,6 +91,14 @@
             $('#waitForProperty').addClass('error');
         }, getRandomResponseTime());
     });
+
+    $('#waitForBlur').blur(function () {
+        setTimeout(function () {
+            $('#waitForBlur').val("blured!");
+        }, getRandomResponseTime());
+
+    });
+
 
     function getRandomResponseTime() {
         var div = $('.current-wait-time');
